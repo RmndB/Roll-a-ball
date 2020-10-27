7e81129e9eb8005eae643985d61f9dcd2f181f1b
philippeHe <phil00henry@gmail.com>
2020-10-27 16:12:19 -0400

    Ajout de nouvelles textures, ajout des items a ramasser, ajout de la caméra qui suit la sphere

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    private GameObject player;
+    public Vector3 offset;
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        offset = new Vector3(0,5,-7);
+    }
+    
+    void Update()
+    {
+        
+        transform.position = (player.transform.position + offset);
+        transform.LookAt(player.transform);
+    }
+}
---
+++ Assets/Scripts/TouchItems.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchItems : MonoBehaviour
+{
+
+    public int countPlayer1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        countPlayer1 = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Item"))
+        {
+            other.gameObject.SetActive(false);
+            countPlayer1++;
+        }
+    }
+    
+}
