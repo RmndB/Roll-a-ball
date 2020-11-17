d126d0ca5fab617404fe8750b71098a48c9f2304
philippeHe <phil00henry@gmail.com>
2020-11-17 11:50:30 -0500

    ajout menus

---
+++ Assets/Scripts/MainMenuCmds.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCmds : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Debug.Log("Quit !!!!!");
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gamePaused = false;
+
+    public GameObject pauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (!gamePaused)
+            {
+                PauseTheGame();
+            }
+        }
+    }
+
+    public void PauseTheGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+    public void ResumeTheGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+
+    public void QuitCurrentGame()
+    {
+        Time.timeScale = 1f;
+        gamePaused = false;
+        Debug.Log("Quit Current Game!!!!!");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + -1);
+    }
+}
