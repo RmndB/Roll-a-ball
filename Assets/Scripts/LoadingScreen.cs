95bc0b916e6d5331cd83f526a8e62134d221eb39
philippeHe <phil00henry@gmail.com>
2020-11-24 11:50:01 -0500

    Création de la LoadingScreen, création des menus pour personnaliser les commandes

---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingScreen : MonoBehaviour
+{
+    private float _timeToWait = 4;
+    private float _progressTime;
+    [SerializeField]
+    private Image _progressBar;
+    public void Start()
+    {
+        _progressTime = 0;
+    }
+
+    public void Update()
+    {
+        _progressTime += Time.deltaTime;
+        var deltaTime = _progressTime / _timeToWait;
+
+        if (deltaTime >= 1)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else
+        {
+            _progressBar.fillAmount = deltaTime;
+        }
+    }
+}
